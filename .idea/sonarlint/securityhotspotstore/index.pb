5433157ef5f0027b7ae52ded4ef9fc74376016de
PabloLopes19 <pabloelopes@hotmail.com>
2024-01-04 20:24:28 -0300

    Segundo commit: algumas alterações bem simples para aprender a desenvolver apps com Java

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,3 @@
+
+o
+?app/src/main/java/com/example/primeiroappjava/MainActivity.java,c/c/cc226940b8ec43b55a7b690a0a866afbbe67b3f5
--- app/src/main/java/com/example/primeiroappjava/MainActivity.java
+++ app/src/main/java/com/example/primeiroappjava/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.primeiroappjava;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.TextView;
+import java.util.Random;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity        {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    Random rand = new Random();
+
+    @SuppressLint("SetTextI18n")
+    public void pegarNumeroAleatorio() {
+        TextView labelResultado = findViewById(R.id.textoResultado);
+        int numeroSelecionado = rand.nextInt(10);
+
+        labelResultado.setText("O número selecionado é: " + numeroSelecionado);
     }
 }
